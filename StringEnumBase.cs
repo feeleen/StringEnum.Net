5768d37e5cfdf0f238124ea995f664f1279b8763
feeleen <feeleen.ru@gmail.com>
2020-12-09 15:42:38 +0300

    initial

---
+++ StringEnumBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace StringEnum
+{
+	public abstract class StringEnumBase<T> : IEquatable<T>
+		where T : StringEnumBase<T>
+	{
+		public string Value { get; protected set; }
+
+		public override string ToString() => this.Value;
+
+		public static T New(string value)
+		{
+			var objValue = Activator.CreateInstance<T>();
+			objValue.Value = value;
+			return objValue;
+		}
+
+		public static List<T> AsList()
+		{
+			return typeof(T)
+				.GetProperties(BindingFlags.Public | BindingFlags.Static)
+				.Where(p => p.PropertyType == typeof(T))
+				.Select(p => (T)p.GetValue(null))
+				.ToList();
+		}
+
+		public static T Parse(string value)
+		{
+			List<T> all = AsList();
+
+			if (!all.Any(a => a.Value == value))
+				throw new InvalidOperationException($"Value \"{value}\" is not a valid value for the type {typeof(T).Name}");
+
+			return all.Single(a => a.Value == value);
+		}
+
+		public bool Equals(T other)
+		{
+			if (other == null) return false;
+			return this.Value == other?.Value;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null) return false;
+			if (obj is T other) return this.Equals(other);
+			return false;
+		}
+
+		public override int GetHashCode() => this.Value.GetHashCode();
+
+		public static bool operator ==(StringEnumBase<T> a, StringEnumBase<T> b) => a?.Equals(b) ?? false;
+
+		public static bool operator !=(StringEnumBase<T> a, StringEnumBase<T> b) => !(a?.Equals(b) ?? false);
+	}
+}
---
+++ UsageExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StringEnum
+{
+	[JsonConverter(typeof(JsonStringEnumConverter<MyPet>))]
+	public class MyPet : StringEnumBase<MyPet>
+	{
+		public static MyPet Cat => New("Cat");
+		public static MyPet Dog => New("Dog");
+		public static MyPet Mouse => New("Mouse");
+	}
+
+	public class Foo
+	{
+		public MyPet pet { get; }
+
+		public Foo(MyPet pet) => this.pet = pet;
+
+		public bool Bar()
+		{
+			if (this.pet == MyPet.Dog || this.pet == MyPet.Mouse)
+				return true;
+			else
+				return false;
+		}
+	}
+}
